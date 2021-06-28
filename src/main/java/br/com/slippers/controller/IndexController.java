29d0579a6310146f3b5a490cc3fa0498b184376e
Andrey <andrey_coradin@hotmail.com>
2021-06-28 18:59:28 -0300

    header

--- src/main/java/br/com/slippers/controller/IndexController.java
+++ src/main/java/br/com/slippers/controller/IndexController.java
@@ -1,9 +1,5 @@
 package br.com.slippers.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
